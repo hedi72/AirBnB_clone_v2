287c81b073d8f04f65baba6d88a340464c0eb857
hedi <113123576+hedi72@users.noreply.github.com>
2023-12-21 13:19:17 +0100

    Update user.py

--- models/user.py
+++ models/user.py
@@ -1,28 +1,24 @@
 #!/usr/bin/python3
-"""This module defines a class User"""
+"""This is the user class"""
 from models.base_model import BaseModel, Base
 from sqlalchemy import Column, String
 from sqlalchemy.orm import relationship
-from os import getenv
+from models.place import Place
+from models.review import Review
 
 
 class User(BaseModel, Base):
-    """This class defines a user by various attributes"""
-
+    """This is the class for user
+    Attributes:
+        email: email address
+        password: password for you login
+        first_name: first name
+        last_name: last name
+    """
     __tablename__ = "users"
-
-    if getenv('HBNB_TYPE_STORAGE') == 'db':
-        email = Column(String(128), nullable=False)
-        password = Column(String(128), nullable=False)
-        first_name = Column(String(128))
-        last_name = Column(String(128))
-        places = relationship("Place", cascade='all, delete, delete-orphan',
-                              backref="user")
-        reviews = relationship("Review", cascade='all, delete, delete-orphan',
-                               backref="user")
-
-    else:
-        email = ''
-        password = ''
-        first_name = ''
-        last_name = ''
+    email = Column(String(128), nullable=False)
+    password = Column(String(128), nullable=False)
+    first_name = Column(String(128), nullable=True)
+    last_name = Column(String(128), nullable=True)
+    places = relationship("Place", cascade="all,delete", backref='user')
+    reviews = relationship("Review", cascade="all,delete", backref='user')
