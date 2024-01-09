1530081fc1aeb16ec7f66d5260623cb953365137
hedi <113123576+hedi72@users.noreply.github.com>
2024-01-09 14:41:35 +0100

    Update amenity.py

--- models/amenity.py
+++ models/amenity.py
@@ -1,20 +1,17 @@
 #!/usr/bin/python3
-""" State Module for HBNB project """
+""" Amenity Module for HBNB project """
 from models.base_model import BaseModel, Base
 from sqlalchemy import Column, String
 from sqlalchemy.orm import relationship
 from os import getenv
-
 
 class Amenity(BaseModel, Base):
     """ Amenity class """
 
     __tablename__ = "amenities"
 
+    name = Column(String(128), nullable=False)
+
     if getenv('HBNB_TYPE_STORAGE') == 'db':
         from models.place import place_amenity
-        name = Column(String(128), nullable=False)
         place_amenities = relationship("Place", secondary=place_amenity)
-
-    else:
-        name = ""
