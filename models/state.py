22c294e95c4f846ddab983714c8809140d035793
hedi <113123576+hedi72@users.noreply.github.com>
2024-01-09 15:38:34 +0100

    Update state.py

--- models/state.py
+++ models/state.py
@@ -1,35 +1,28 @@
 #!/usr/bin/python3
-""" State Module for HBNB project """
+"""This is the state class"""
 from models.base_model import BaseModel, Base
 from sqlalchemy import Column, String
 from sqlalchemy.orm import relationship
 from models.city import City
-from os import getenv
-
-storage_type = getenv("HBNB_TYPE_STORAGE")
 
 
 class State(BaseModel, Base):
-    """ State class """
+    """This is the class for State
+    Attributes:
+        name: input name
+    """
+    __tablename__ = "states"
+    name = Column(String(128), nullable=False)
+    cities = relationship("City", cascade="all,delete", backref="state")
 
-    __tablename__ = 'states'
-    if storage_type == "db":
-        name = Column(String(128), nullable=False)
-        cities = relationship('City', cascade="all,delete", backref="state")
-    else:
-        name = ""
-        # DONE: for FileStorage: getter attribute cities that
-        # returns the list of City instances with state_id equals
-        # to the current State.id => It will be the FileStorage
-        # relationship between State and City
-
-        @property
-        def cities(self):
-            """getter docuemnt"""
-            from models import storage
-            citiesList = []
-            citiesAll = storage.all(City)
-            for city in citiesAll.values():
-                if city.state_id == self.id:
-                    citiesList.append(city)
-            return citiesList
+    @property
+    def cities(self):
+        """  getter attribute cities that returns the list of
+        City instances with state_id equals to the current State.id """
+        from models import storage
+        allcities = storage.all(City)
+        c = []
+        for k, v in allcities.items():
+            if v.state_id == self.id:
+                c.append(v)
+        return c
