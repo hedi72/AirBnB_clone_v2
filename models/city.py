498d287d53184274c7b79d443426e21b8df410b5
hedi <113123576+hedi72@users.noreply.github.com>
2024-01-09 15:37:02 +0100

    Update city.py

--- models/city.py
+++ models/city.py
@@ -1,21 +1,18 @@
 #!/usr/bin/python3
-""" City Module for HBNB project """
+"""This is the city class"""
 from models.base_model import BaseModel, Base
 from sqlalchemy import Column, String, ForeignKey
 from sqlalchemy.orm import relationship
-from os import getenv
-
-storage_type = getenv("HBNB_TYPE_STORAGE")
+from models.place import Place
 
 
 class City(BaseModel, Base):
-    """The city class, contains state ID and name"""
-
+    """This is the class for City
+    Attributes:
+        state_id: The state id
+        name: input name
+    """
     __tablename__ = "cities"
-    if storage_type == "db":
-        name = Column(String(128), nullable=False)
-        state_id = Column(String(60), ForeignKey("states.id"), nullable=False)
-        places = relationship('Place', cascade="all,delete", backref="cities")
-    else:
-        name = ""
-        state_id = ""
+    state_id = Column(String(60), ForeignKey("states.id"), nullable=False)
+    name = Column(String(128), nullable=False)
+    places = relationship("Place", cascade="all,delete", backref='cities')
