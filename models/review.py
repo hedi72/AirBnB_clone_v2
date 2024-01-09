c0d2ffe00ae89417d253d6cdb625ee73aa2cf037
hedi <113123576+hedi72@users.noreply.github.com>
2024-01-09 15:21:17 +0100

    Update review.py

--- models/review.py
+++ models/review.py
@@ -1,24 +1,21 @@
 #!/usr/bin/python3
 """ Review module for the HBNB project """
 from models.base_model import BaseModel, Base
-from sqlalchemy import Column, String, ForeignKey
+from sqlalchemy import Column, String, Text, ForeignKey
 from sqlalchemy.orm import relationship
 from os import getenv
+
+storage_type = getenv("HBNB_TYPE_STORAGE")
 
 
 class Review(BaseModel, Base):
     """ Review classto store review information """
+    __tablename__ = 'reviews'
 
-    __tablename__ = "reviews"
-
-    if getenv('HBNB_TYPE_STORAGE') == 'db':
-        place_id = Column(String(60), ForeignKey("places.id"),
-                          nullable=False)
+    if storage_type == "db":
+        place_id = Column(String(60), ForeignKey("places.id"), nullable=False)
         user_id = Column(String(60), ForeignKey("users.id"), nullable=False)
-        text = Column(String(1024), nullable=False)
-        user = relationship("User", back_populates="reviews")
-        place = relationship("Place", back_populates="reviews")
-
+        text = Column(Text(1024), nullable=False)
     else:
         place_id = ""
         user_id = ""
